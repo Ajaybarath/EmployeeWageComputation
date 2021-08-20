8cf8dad22586e8948a615825ad9f3d0e1a0a6511
ajaybarath <ajaybarath3@gmail.com>
2021-08-20 16:18:38 +0530

    part time hours added

--- src/main/java/com/employeeWageCalculator/EmployeeWage.java
+++ src/main/java/com/employeeWageCalculator/EmployeeWage.java
@@ -4,14 +4,25 @@ public class EmployeeWage {
 	
 	private static final int EmployeeWagePerHour = 20;
 	private static final int FullDayHours = 8;
+	private static final int PartTimeHours = 4;
 
 	public static void main(String args[]) {
 		
-		int employeeStatus = (int) (Math.floor(Math.random() * 10) % 2);
+		int employeeAttendenceStatus = (int) (Math.floor(Math.random() * 10) % 2);  // 1 means present, 0 means absent
+		
+		int employeeWorkStatus = (int) (Math.floor(Math.random() * 10) % 2);  // 1 means fulltime, 0 means partime
+		
 		int employeeWage = 0;
 		
-		if (employeeStatus == 1) {
-			employeeWage = EmployeeWagePerHour * FullDayHours;
+		if (employeeAttendenceStatus == 1) {
+			
+			if (employeeWorkStatus == 1) {
+				employeeWage = EmployeeWagePerHour * FullDayHours;
+			}
+			else {
+				employeeWage = EmployeeWagePerHour * PartTimeHours;
+			}
+			
 		}
 		else {
 			employeeWage = 0;
