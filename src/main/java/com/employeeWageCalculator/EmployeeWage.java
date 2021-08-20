b2acf1dae5d56df67398bc4c593634726e1b995e
ajaybarath <ajaybarath3@gmail.com>
2021-08-20 16:25:33 +0530

    switch case implemented

--- src/main/java/com/employeeWageCalculator/EmployeeWage.java
+++ src/main/java/com/employeeWageCalculator/EmployeeWage.java
@@ -1,34 +1,37 @@
 package com.employeeWageCalculator;
 
 public class EmployeeWage {
-	
+
 	private static final int EmployeeWagePerHour = 20;
 	private static final int FullDayHours = 8;
 	private static final int PartTimeHours = 4;
 
 	public static void main(String args[]) {
-		
-		int employeeAttendenceStatus = (int) (Math.floor(Math.random() * 10) % 2);  // 1 means present, 0 means absent
-		
-		int employeeWorkStatus = (int) (Math.floor(Math.random() * 10) % 2);  // 1 means fulltime, 0 means partime
-		
+
+		int employeeAttendenceStatus = (int) (Math.floor(Math.random() * 10) % 2); // 1 means present, 0 means absent
+
+		int employeeWorkStatus = (int) (Math.floor(Math.random() * 10) % 2) + 1; // 1 means fulltime, 0 means partime
+
 		int employeeWage = 0;
-		
-		if (employeeAttendenceStatus == 1) {
-			
-			if (employeeWorkStatus == 1) {
-				employeeWage = EmployeeWagePerHour * FullDayHours;
-			}
-			else {
-				employeeWage = EmployeeWagePerHour * PartTimeHours;
-			}
-			
+
+		if (employeeAttendenceStatus == 0) {
+			employeeWorkStatus = 0;
 		}
-		else {
+
+		switch (employeeWorkStatus) {
+		case (1):
+			employeeWage = EmployeeWagePerHour * FullDayHours;
+			break;
+
+		case (2):
+			employeeWage = EmployeeWagePerHour * PartTimeHours;
+			break;
+
+		default:
 			employeeWage = 0;
 		}
-		
+
 		System.out.println("Employee wage calculated is " + employeeWage);
-		
+
 	}
 }
